370f4348ce9a96f4d8f0a117b98d1ef03691387c
Margarita Miranda <mmiranda@Margaritas-MacBook-Pro.local>
2023-02-03 13:41:30 -0800

    remove logs

--- utils/utils.js
+++ utils/utils.js
@@ -23,8 +23,6 @@ export const getOccurrences = (letter, word) => {
 
 export const evaluation = (index, wordArray, winningWord, winningWordArray) => {
     let letter = wordArray[index]
-
-    console.log("   evaluation: (index) " + index + ", (entered) " + letter + ", (winning) " + winningWordArray[index]);
     // if letter is in the right spot - correct
     if (winningWordArray[index] == letter) {
         return utilStyles.wordBoxCorrect;
@@ -59,8 +57,7 @@ export const evaluations = (wordArray, winningWord, winningWordArray) => {
             return c;
         }
     });
-    console.log("evaluation for - " + word);
-    console.log(state);
+
     return state;
 }
 
