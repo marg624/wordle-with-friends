db3fea2c2484aa42685c95c9377c31757ac32269
marg624 <marg624@gmail.com>
2023-03-13 20:54:49 -0700

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
 export const EXAMPLE_PATH = 'w/ friends'
 export const CMS_NAME = 'Markdown'
 export const HOME_OG_IMAGE_URL =
-  'https://wordle-with-friends.s3.us-west-2.amazonaws.com/cover.png'
+  'https://wordle-with-friends.s3.us-west-2.amazonaws.com/wordle.png'
